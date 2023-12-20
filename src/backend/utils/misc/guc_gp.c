f41bb43087c773318a1f5266f43020aa4fd97131
hari krishna <hmaddileti@vmware.com>
2023-12-20 21:11:36 +0530

    Add new GUC gp_max_system_slices

--- src/backend/utils/misc/guc_gp.c
+++ src/backend/utils/misc/guc_gp.c
@@ -4278,7 +4278,17 @@ struct config_int ConfigureNamesInt_gp[] =
 	},
 
 	{
-		{"gp_max_slices", PGC_SUSET, PRESET_OPTIONS,
+		{"gp_max_slices", PGC_USERSET, PRESET_OPTIONS,
+			gettext_noop("Maximum slices for a single query"),
+			NULL,
+			GUC_NOT_IN_SAMPLE
+		},
+		&gp_max_slices,
+		0, 0, INT_MAX, NULL, NULL
+	},
+
+	{
+		{"gp_max_system_slices", PGC_SUSET, PRESET_OPTIONS,
 			gettext_noop("Maximum slices for a single query"),
 			NULL,
 			GUC_NOT_IN_SAMPLE
